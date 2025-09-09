7d163956a1567400ff961837accb2a7752c49331
Tom Manning <tom@manningetal.com>
2025-09-09 11:21:49 -0400

    compare server_image_number to restored_image_number (when to update display)

--- display_app.py
+++ display_app.py
@@ -3,6 +3,7 @@ from ftplib import FTP
 import machine
 import ujson as json
 import tinypico
+from epaper10in2 import EPD
 
 def setup_station(ssid, password):
    sta_if = network.WLAN(network.STA_IF)
@@ -51,9 +52,29 @@ def read_config(filename="config.json"):
       print(f"Error loading {filename}: {e}")
    return config
 
+def restore_state():
+   tmp = None
+   try:
+      tmp = json.loads(rtc.memory().decode)
+      print(f"RTC.memory={tmp}")
+      if type(tmp) is not dict:
+            print(f"Warning (restore_state): RTC.memory={tmp} is not a dictionary")
+            tmp = None
+   except:
+      print(f"Warning (restore_state): RTC.memory={tmp} was not JSON")
+   return tmp
+
 
 def main():
    tinypico.set_dotstar_power(False)
+   rtc = machine.RTC()
+   sck = machine.Pin(18)
+   miso = machine.Pin(19)
+   mosi = machine.Pin(23)
+   dc = machine.Pin(15)
+   cs = machine.Pin(26)
+   rst = machine.Pin(25)
+   busy = machine.Pin(27)
 
    config = read_config()
    if config is None:
@@ -73,10 +94,33 @@ def main():
       print(f"{e} on cwd to epaper")
       my_deep_sleep(config['sleep'])
 
-   image_number = get_image_number(ftp)
+   server_image_number = get_image_number(ftp)
    ftp.quit()
-   if image_number is None:
-      print(f"Getting the image number failed")
+
+   restored_state = restore_state()
+   if restored_state is not None and 'img_num' in restored_state:
+      restored_image_number = restored_state['img_num']
+   else:
+      restored_image_number = None
+
+   if restored_image_number == server_image_number:
+      print(f"{restored_image_number=} matches {server_image_number=}: not updating display")
       my_deep_sleep(config['sleep'])
-   print(f"image_number={image_number}")
+   else:
+      print(f"changing display from {restored_image_number=} to {server_image_number=}")
+      rtc.memory(bytearray(json.dumps({"img_num": server_image_number}).encode()))
+
+      # spi = machine.SPI(2, baudrate=4000000, polarity=0, phase=0, sck=sck, miso=miso, mosi=mosi)
+      # e = EPD(spi, cs, dc, rst, busy)
+      # if not e.init():
+      #    print("e-paper init failed")
+      # with open("open-welcome.bin", 'rb') as file:
+      #    byte_array = bytearray(file.read())
+      # e.display(byte_array)
+
+
+   if 0:
+      state = {"img_num": 3}
+      rtc.memory(bytearray(json.dumps(state).encode())) # store state as JSON in RTC memory
+      restored_image_number = json.loads(rtc.memory().decode())
 
